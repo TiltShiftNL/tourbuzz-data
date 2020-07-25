12b4748aac378540ca48f84a1b9ca5255b42fde5
milovanderlinden <milo@dogodigi.net>
2020-07-25 14:47:57 +0200

    Bypass certificate check

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 export PGPASS=$POSTGRES_PASSWORD
 export PGPASSWORD=$POSTGRES_PASSWORD
 
-wget -O /data/data.osm.pbf $PBF_URL
+wget --no-check-certificate -O /data/data.osm.pbf $PBF_URL
 
 osm2pgsql --username $POSTGRES_USER --database $POSTGRES_DB --host $POSTGRES_HOST --port $POSTGRES_PORT --create --slim -G --hstore --tag-transform-script /data/osm-data.lua -C 2048 --number-processes ${THREADS:-4} -S /data/osm-data.style /data/data.osm.pbf
